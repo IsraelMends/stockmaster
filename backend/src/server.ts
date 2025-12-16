9435f35ef1ef41c3ec87f108520b606fe39bbbf0
IsraelMends <iomendes505@gmail.com>
2025-12-16 14:46:51 -0300

    feat: integrate categories routes into Express server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,21 +4,27 @@ import cors from 'cors' //Permite que outros sites acessem minha API
 
 import dotenv from 'dotenv' //Carrega as informações do arquivo .env
 
+import { router as categoryRouter } from './routes/categoriesRoutes.js'
+
 dotenv.config()
 
-const app  = express();
+const app = express();
+
+
 
 app.use(cors());
 
 app.use(express.json());
 
-app.get('/', (req,res) =>{
-    res.json({message:'API Funcionando'})
+app.use(categoryRouter)
+
+app.get('/', (req, res) => {
+    res.json({ message: 'API Funcionando' })
 })
 
 const PORT = process.env.PORT || 3333;
 
-app.listen(PORT, () =>{
+app.listen(PORT, () => {
     //Função executada quando o servidor está ligado
     console.log(`HTTP Server is running in port ${PORT}`)
 })
