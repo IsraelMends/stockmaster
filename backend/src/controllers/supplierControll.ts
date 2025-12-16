3836c5a09004d7a71db7d3d2a3f4509a9d080fdc
IsraelMends <iomendes505@gmail.com>
2025-12-16 16:50:44 -0300

    feat: implement supplier controller with CRUD operations for suppliers

---
+++ backend/src/controllers/supplierControll.ts
@@ -0,0 +1,73 @@
+/**
+ * Controller for Supplliers
+ */
+
+import { Request, Response } from 'express'
+
+import { prisma } from '../lib/prisma.js'
+
+// GET /suppliers
+const index = async (req: Request, res: Response) => {
+    const suppliers = await prisma.supplier.findMany()
+    return res.json(suppliers)
+}
+
+// GET /suppliers/:id
+const show = async (req: Request, res: Response) => {
+    const id = Number(req.params.id)
+
+    const supplier = await prisma.supplier.findUnique({
+        where: { id: id }
+    })
+
+    return res.json(supplier)
+}
+
+// POST /suppliers
+const create = async (req: Request, res: Response) => {
+    const { name, cnpj, email, phone, address } = req.body
+    const supplier = await prisma.supplier.create({
+        data: {
+            name,
+            cnpj,
+            email,
+            phone,
+            address
+        }
+    })
+
+    return res.status(201).json(supplier)
+}
+
+// PUT /supplier/:id
+const update = async (req: Request, res: Response) => {
+    const { name, cnpj, email, phone, address } = req.body
+
+    const id = Number(req.params.id)
+
+    const supplier = await prisma.supplier.update({
+        where: { id: id },
+        data: {
+            name,
+            cnpj,
+            email,
+            phone,
+            address
+        }
+    })
+
+    return res.json(supplier)
+}
+
+// DELETE /supplier/:id
+const destroy = async (req: Request, res: Response) => {
+    const id = Number(req.params.id)
+
+    await prisma.supplier.delete({
+        where: { id: id }
+    })
+
+    return res.status(204).send()
+}
+
+export { index, show, create, update, destroy }
