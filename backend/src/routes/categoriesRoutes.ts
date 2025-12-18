49e260684bf1511cfc9eba26c9153a561e33ffff
IsraelMends <iomendes505@gmail.com>
2025-12-18 14:39:31 -0300

    feat: update category routes to use new validation schema for updates and simplify delete operation

--- backend/src/routes/categoriesRoutes.ts
+++ backend/src/routes/categoriesRoutes.ts
@@ -4,7 +4,8 @@ import { index, show, create, update, destroy } from '../controllers/categoryCon
 
 import { authMiddleware } from '../middlewares/authMiddleware.js'
 
-import { createCategorySchema } from '../validations/categoryValidation.js'
+import { createCategorySchema, updateCategorySchema } from '../validations/categoryValidation.js'
+
 import { validate } from '../middlewares/validationMiddleware.js';
 
 const router = Router();
@@ -15,8 +16,8 @@ router.get('/categories/:id', show)
 
 router.post('/categories', authMiddleware, validate(createCategorySchema), create)
 
-router.put('/categories/:id', authMiddleware, validate(createCategorySchema), update)
+router.put('/categories/:id', authMiddleware, validate(updateCategorySchema), update)
 
-router.delete('/categories/:id', authMiddleware, validate(createCategorySchema), destroy)
+router.delete('/categories/:id', authMiddleware, destroy)
 
 export { router }
