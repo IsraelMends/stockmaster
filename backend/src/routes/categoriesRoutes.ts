af8477d51b1ef23e26872aa62d099cfd8f2eb542
IsraelMends <iomendes505@gmail.com>
2025-12-18 10:47:05 -0300

    feat: integrate validation middleware for category routes to ensure request body validation on create, update, and delete operations

--- backend/src/routes/categoriesRoutes.ts
+++ backend/src/routes/categoriesRoutes.ts
@@ -1,6 +1,11 @@
 import { Router } from 'express'
+
 import { index, show, create, update, destroy } from '../controllers/categoryController.js'
+
 import { authMiddleware } from '../middlewares/authMiddleware.js'
+
+import { createCategorySchema } from '../validations/categoryValidation.js'
+import { validate } from '../middlewares/validationMiddleware.js';
 
 const router = Router();
 
@@ -8,10 +13,10 @@ router.get('/categories', index)
 
 router.get('/categories/:id', show)
 
-router.post('/categories', authMiddleware, create)
+router.post('/categories', authMiddleware, validate(createCategorySchema), create)
 
-router.put('/categories/:id', authMiddleware, update)
+router.put('/categories/:id', authMiddleware, validate(createCategorySchema), update)
 
-router.delete('/categories/:id', authMiddleware, destroy)
+router.delete('/categories/:id', authMiddleware, validate(createCategorySchema), destroy)
 
 export { router }
