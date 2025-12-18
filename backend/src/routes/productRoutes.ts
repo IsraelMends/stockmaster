e15dc599b7af8fe3ae0bc55e5f1ba4dc8c4a480c
IsraelMends <iomendes505@gmail.com>
2025-12-18 14:39:17 -0300

    feat: enhance product routes with validation middleware for create and update operations

--- backend/src/routes/productRoutes.ts
+++ backend/src/routes/productRoutes.ts
@@ -1,6 +1,12 @@
 import { Router } from 'express'
+
 import { index, show, create, update, destroy } from '../controllers/productController.js'
+
 import { authMiddleware } from '../middlewares/authMiddleware.js'
+
+import { updateProductsSchema, createProductsSchema } from '../validations/productsValidation.js'
+
+import { validate } from '../middlewares/validationMiddleware.js'
 
 const router = Router()
 
@@ -8,9 +14,9 @@ router.get('/products', index)
 
 router.get('/products/:id', show)
 
-router.post('/products', authMiddleware, create)
+router.post('/products', authMiddleware, validate(createProductsSchema), create)
 
-router.put('/products/:id', authMiddleware, update)
+router.put('/products/:id', authMiddleware, validate(updateProductsSchema), update)
 
 router.delete('/products/:id', authMiddleware, destroy)
 
