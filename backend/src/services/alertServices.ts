05cfb57f2744ea307f12b840eafd7e0336b10781
Israel Mends <israel.mendes@a.unileste.edu.br>
2025-12-29 12:14:39 -0300

    feat: enhance low stock alert service to create alerts for low stock and mark them as read when stock is sufficient

--- backend/src/services/alertServices.ts
+++ backend/src/services/alertServices.ts
@@ -10,6 +10,42 @@ export async function checkAndCreateLowStockAlert(productId: number) {
     return null;
   }
 
-  
+  //Verify if stock is low or equal minimum
+  if (findProduct.currentStock <= findProduct.minimumStock) {
+    //Search if there is an unread alert
+    const existingAlert = await prisma.alert.findFirst({
+      where: {
+        productId: productId,
+        type: "LOW_STOCK",
+        read: false,
+      },
+    });
 
+    if (!existingAlert) {
+      const alert = await prisma.alert.create({
+        data: {
+          productId: productId,
+          type: "LOW_STOCK",
+          message: `Low stock! The product “${findProduct.name}” has ${findProduct.currentStock} ${findProduct.unit}, below the minimum of ${findProduct.minimumStock} ${findProduct.unit}.`,
+        },
+      });
+
+      return alert;
+    }
+
+    return existingAlert;
+  } else {
+    //If inventory has returned to normal, mark alerts as read.
+    await prisma.alert.updateMany({
+      where: {
+        productId: productId,
+        type: "LOW_STOCK",
+        read: false,
+      },
+      data: {
+        read: true,
+      },
+    });
+    return null;
+  }
 }
