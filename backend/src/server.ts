6664ec9cef2c23494d05623e828c4fbf31e0c040
Israel Mends <israel.mendes@a.unileste.edu.br>
2025-12-29 19:19:13 -0300

    chore: standardize import statements and add alerts router to server configuration

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,42 +1,43 @@
-import express from 'express';
+import express from "express";
 
-import cors from 'cors' //Permite que outros sites acessem minha API
+import cors from "cors"; //Permite que outros sites acessem minha API
 
-import dotenv from 'dotenv' //Carrega as informações do arquivo .env
+import dotenv from "dotenv"; //Carrega as informações do arquivo .env
 
-import { router as authRouter } from './routes/authRoutes.js'
+import { router as authRouter } from "./routes/authRoutes.js";
 
-import { router as categoryRouter } from './routes/categoriesRoutes.js'
+import { router as categoryRouter } from "./routes/categoriesRoutes.js";
 
-import { router as productsRouter } from './routes/productRoutes.js'
+import { router as productsRouter } from "./routes/productRoutes.js";
 
-import { router as supplierRouter } from './routes/suppliersRoutes.js'
+import { router as supplierRouter } from "./routes/suppliersRoutes.js";
 
-import { router as stockMovimentRouter } from './routes/stockMovementsRoutes.js'
+import { router as stockMovimentRouter } from "./routes/stockMovementsRoutes.js";
 
-dotenv.config()
+import { router as alertsRouter } from "./routes/alertRoutes.js";
+
+dotenv.config();
 
 const app = express();
-
-
 
 app.use(cors());
 
 app.use(express.json());
 
-app.use(authRouter)
-app.use(categoryRouter)
-app.use(productsRouter)
-app.use(supplierRouter)
-app.use(stockMovimentRouter)
+app.use(authRouter);
+app.use(categoryRouter);
+app.use(productsRouter);
+app.use(supplierRouter);
+app.use(stockMovimentRouter);
+app.use(alertsRouter);
 
-app.get('/', (req, res) => {
-    res.json({ message: 'API Funcionando' })
-})
+app.get("/", (req, res) => {
+  res.json({ message: "API Funcionando" });
+});
 
 const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
-    //Função executada quando o servidor está ligado
-    console.log(`HTTP Server is running in port ${PORT}`)
-})
+  //Função executada quando o servidor está ligado
+  console.log(`HTTP Server is running in port ${PORT}`);
+});
