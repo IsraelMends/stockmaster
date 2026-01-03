88934687249d8f5941ef24f645c69cdda5f9249d
Israel Mends <israel.mendes@a.unileste.edu.br>
2026-01-02 23:47:47 -0300

    feat: implement pagination for supplier listing in supplierController to enhance data retrieval and response structure

--- backend/src/controllers/supplierControll.ts
+++ backend/src/controllers/supplierControll.ts
@@ -2,72 +2,91 @@
  * Controller for Supplliers
  */
 
-import { Request, Response } from 'express'
+import { Request, Response } from "express";
 
-import { prisma } from '../lib/prisma.js'
+import { prisma } from "../lib/prisma.js";
 
 // GET /suppliers
-const index = async (res: Response) => {
-    const suppliers = await prisma.supplier.findMany()
-    return res.json(suppliers)
-}
+const index = async (req: Request, res: Response) => {
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+
+  const skip = (page - 1) * limit;
+  const total = await prisma.supplier.count();
+
+  const totalPages = Math.ceil(total / limit);
+
+  const suppliers = await prisma.supplier.findMany({
+    skip: skip,
+    take: limit,
+  });
+  return res.json({
+    data: suppliers,
+    pagination: {
+      page: page,
+      limit: limit,
+      total: total,
+      totalPages: totalPages,
+    },
+  });
+};
 
 // GET /suppliers/:id
 const show = async (req: Request, res: Response) => {
-    const id = Number(req.params.id)
+  const id = Number(req.params.id);
 
-    const supplier = await prisma.supplier.findUnique({
-        where: { id: id }
-    })
+  const supplier = await prisma.supplier.findUnique({
+    where: { id: id },
+  });
 
-    return res.json(supplier)
-}
+  return res.json(supplier);
+};
 
 // POST /suppliers
 const create = async (req: Request, res: Response) => {
-    const { name, cnpj, email, phone, address } = req.body
-    const supplier = await prisma.supplier.create({
-        data: {
-            name,
-            cnpj,
-            email,
-            phone,
-            address
-        }
-    })
+  const { name, cnpj, email, phone, address } = req.body;
+  const supplier = await prisma.supplier.create({
+    data: {
+      name,
+      cnpj,
+      email,
+      phone,
+      address,
+    },
+  });
 
-    return res.status(201).json(supplier)
-}
+  return res.status(201).json(supplier);
+};
 
 // PUT /supplier/:id
 const update = async (req: Request, res: Response) => {
-    const { name, cnpj, email, phone, address } = req.body
+  const { name, cnpj, email, phone, address } = req.body;
 
-    const id = Number(req.params.id)
+  const id = Number(req.params.id);
 
-    const supplier = await prisma.supplier.update({
-        where: { id: id },
-        data: {
-            name,
-            cnpj,
-            email,
-            phone,
-            address
-        }
-    })
+  const supplier = await prisma.supplier.update({
+    where: { id: id },
+    data: {
+      name,
+      cnpj,
+      email,
+      phone,
+      address,
+    },
+  });
 
-    return res.json(supplier)
-}
+  return res.json(supplier);
+};
 
 // DELETE /supplier/:id
 const destroy = async (req: Request, res: Response) => {
-    const id = Number(req.params.id)
+  const id = Number(req.params.id);
 
-    await prisma.supplier.delete({
-        where: { id: id }
-    })
+  await prisma.supplier.delete({
+    where: { id: id },
+  });
 
-    return res.status(204).send()
-}
+  return res.status(204).send();
+};
 
-export { index, show, create, update, destroy }
+export { index, show, create, update, destroy };
